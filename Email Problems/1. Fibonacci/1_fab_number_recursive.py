ed07cebddd6f4f89ad20babeff80db37ddeae02c
Shekhar <shekharkrishnashrestha@gmail.com>
2020-09-16 03:03:09 -0400

    init

---
+++ Email Problems/1. Fibonacci/1_fab_number_recursive.py
@@ -0,0 +1,49 @@
+import unittest
+
+def fib(n):
+    # compute the nth Fibonacci number    
+    if n in [1, 0]:
+        return n
+    return fib(n - 1) + fib(n - 2)
+
+    #return -1
+
+# Tests
+class Test(unittest.TestCase):
+
+    def test_zeroth_fibonacci(self):
+        actual = fib(0)
+        expected = 0
+        self.assertEqual(actual, expected)
+
+    def test_first_fibonacci(self):
+        actual = fib(1)
+        expected = 1
+        self.assertEqual(actual, expected)
+
+    def test_second_fibonacci(self):
+        actual = fib(2)
+        expected = 1
+        self.assertEqual(actual, expected)
+
+    def test_third_fibonacci(self):
+        actual = fib(3)
+        expected = 2
+        self.assertEqual(actual, expected)
+
+    def test_fifth_fibonacci(self):
+        actual = fib(5)
+        expected = 5
+        self.assertEqual(actual, expected)
+
+    def test_tenth_fibonacci(self):
+        actual = fib(10)
+        expected = 55
+        self.assertEqual(actual, expected)
+
+    def test_negative_fibonacci(self):
+        with self.assertRaises(Exception):
+            fib(-1)
+
+
+unittest.main(verbosity=2)
