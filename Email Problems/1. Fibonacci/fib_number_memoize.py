5645b8b40074834e35130bb57a9a9d6cfc2c629e
Shekhar <shekharkrishnashrestha@gmail.com>
2020-09-16 05:34:52 -0400

    Test Case Inbuilt in fib_number_memoize.py

--- Email Problems/1. Fibonacci/fib_number_memoize.py
+++ Email Problems/1. Fibonacci/fib_number_memoize.py
@@ -1,4 +1,4 @@
-#import unittest
+import unittest
 
 
 class Fibber(object):
@@ -29,3 +29,50 @@ print("Fib : ")
 fibaaaaaaa = Fibber()
 print(fibaaaaaaa)
 print(fibaaaaaaa.fib(7))
+
+# Can define the class here?
+class Test(unittest.TestCase):
+    #fi = Fibber() # i was not able to make it use in the below defination so labor work done below
+    def test_zeroth_fibonacci(self):    
+        #fi = Fibber()    
+        #actual = fi.fib(0)
+        actual = fibaaaaaaa.fib(0)
+        expected = 0
+        self.assertEqual(actual, expected)
+
+    def test_first_fibonacci(self):
+        #fi = Fibber()
+        actual = fibaaaaaaa.fib(1)
+        expected = 1
+        self.assertEqual(actual, expected)
+
+    def test_second_fibonacci(self):
+        fi = Fibber()
+        actual = fibaaaaaaa.fib(2)
+        expected = 1
+        self.assertEqual(actual, expected)
+
+    def test_third_fibonacci(self):
+        fi = Fibber()
+        actual = fibaaaaaaa.fib(3)
+        expected = 2
+        self.assertEqual(actual, expected)
+
+    def test_fifth_fibonacci(self):
+        fi = Fibber()
+        actual = fibaaaaaaa.fib(5)
+        expected = 5
+        self.assertEqual(actual, expected)
+
+    def test_tenth_fibonacci(self):
+        fi = Fibber()
+        actual = fibaaaaaaa.fib(10)
+        expected = 55
+        self.assertEqual(actual, expected)
+
+    def test_negative_fibonacci(self):
+        with self.assertRaises(Exception):
+            fib(-1)
+
+
+unittest.main(verbosity=2) 
